2e8117f89723bf160e1e617f4c86374ded788bfb
aishu <aishumeela@gmail.com>
2026-01-02 09:38:31 +0530

    firstcommit

---
+++ src/StringMethods.java
@@ -0,0 +1,53 @@
+public class StringMethods {
+
+    public static void main(String[] args) {
+
+  /*      String s="Computer Science Engineer";
+
+        s=s.concat(  " Delta ");
+
+       System.out.println(s);
+    }
+
+   */
+  //     String s = "computer Science And Engineering DELTA";
+  //     System.out.println(s.substring(0, 16));
+
+ //       String s1=new String("JAVA");
+ //       String s2=new String("JAVA");
+
+ //       System.out.println(s1==s2);
+  //      System.out.println(s2.equals(s2));
+ //       String s1=new String("JAVA");
+  //      String s2=new String("JAVA");
+  //      System.out.println(s1.equalsIgnoreCase(s2));
+
+  //       String s1 = "JAVA";
+ //        System.out.println(s1.toUpperCase());
+  //       System.out.println(s1.toLowerCase());
+
+  //      String s=" Hello World ";
+  //      System.out.println(s.trim());
+
+   //     String s ="Automation Testing";
+   //     System.out.println(s.contains("Testing"));
+
+  //      String s="Computer Science and Engineering";
+  //      System.out.println(s.contains("Engineering"));
+
+ //       String s="Java Programming";
+   //     System.out.println(s.startsWith("Java"));
+ //       System.out.println(s.endsWith("ing"));
+
+
+String s="Java,Python,c++";
+String[] arr =s.split(",");
+for(String lang:arr)
+{
+    System.out.print(lang+" ");
+}
+
+
+    }
+}
+
